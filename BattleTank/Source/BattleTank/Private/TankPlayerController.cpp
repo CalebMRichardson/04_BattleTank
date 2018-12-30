aeb50283996efbc0e78509a61eeaf5a5718678e6
CalebMRichardson <cmrichardson1993@gmail.com>
2018-12-30 13:40:52 -0800

    Fleshing out TankPlayerController.cpp

--- BattleTank/Source/BattleTank/Private/TankPlayerController.cpp
+++ BattleTank/Source/BattleTank/Private/TankPlayerController.cpp
@@ -2,6 +2,19 @@
 
 #include "TankPlayerController.h"
 
+void ATankPlayerController::BeginPlay() {
+	Super::BeginPlay();
+
+	UE_LOG(LogTemp, Warning, TEXT("Player conroller BeginPlay()"));
+	
+	auto controlledTank = GetControlledTank();
+	
+	if (controlledTank) {
+		UE_LOG(LogTemp, Warning, TEXT("Possed Tank: %s"), *(controlledTank->GetName()));
+	} else {
+		UE_LOG(LogTemp, Error, TEXT("No possed Tank."));
+	}
+}
 
 ATank* ATankPlayerController::GetControlledTank() const {
 	
