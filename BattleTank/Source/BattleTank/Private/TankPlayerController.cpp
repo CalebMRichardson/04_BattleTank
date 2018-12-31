ef3995bf56ec29738d5c0203e4c5f44d58c75a06
CalebMRichardson <cmrichardson1993@gmail.com>
2018-12-30 16:50:47 -0800

    Found Deprojected Screen-to-world-position.

--- BattleTank/Source/BattleTank/Private/TankPlayerController.cpp
+++ BattleTank/Source/BattleTank/Private/TankPlayerController.cpp
@@ -43,13 +43,17 @@ void ATankPlayerController::AimTowardsCrosshair() {
 // Get world location of LineTrace through crosshair, true if it hits landscape
 bool ATankPlayerController::GetSightRayHitLocation(FVector &OutHitLocation) const {
 
-	// Find the crosshair position in pixel coord
+	// Find the crosshair position in pixel coordinates
 	int32 ViewportSizeX, ViewportSizeY; 
 	GetViewportSize(ViewportSizeX, ViewportSizeY);
 	auto ScreenLocation = FVector2D(ViewportSizeX * CrossHairXLocation, ViewportSizeY * CrossHairYLocation);
-	
-	//TODO REMOVE ME
-	UE_LOG(LogTemp, Warning, TEXT("Screen Location: %s"), *(ScreenLocation.ToString()))
+
+	FVector CameraWorldLocation; 
+	FVector LookDirection; 
+
+	if (GetLookDirection(ScreenLocation, LookDirection)) {
+		UE_LOG(LogTemp, Warning, TEXT("Look Direction: %s"), *LookDirection.ToString()); 
+	}
 
 	// "Deproject" the screen position of the crosshair to a world direction
 	// Line-Trace along that look direction, and see what we hit ( up to a max range )
@@ -57,3 +61,12 @@ bool ATankPlayerController::GetSightRayHitLocation(FVector &OutHitLocation) cons
 	return true;
 }
 
+bool ATankPlayerController::GetLookDirection(FVector2D ScreenLocation, FVector& LookDirection) const {
+	
+	FVector CameraWorldLocation; // To be discarded
+
+	return DeprojectScreenPositionToWorld(ScreenLocation.X, 
+			ScreenLocation.Y, 
+			CameraWorldLocation, 
+			LookDirection); 
+}
